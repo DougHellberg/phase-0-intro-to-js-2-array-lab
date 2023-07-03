7dfd88b4018284ca986ef73f6d296d13280c1ea6
Douglas Hellberg <DougHellberg@gmail.com>
2023-07-02 20:07:24 -0400

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]; 
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const appendCat = cats.slice();
+    appendCat.push(name);
+    return appendCat;
+}
+function prependCat(name){
+    const prependCat = cats.slice();
+    prependCat.unshift(name);
+    return prependCat;
+}
+function removeLastCat(){
+    const removeLastCat = cats.slice();
+    removeLastCat.pop();
+    return removeLastCat;
+}
+function removeFirstCat(){
+    const removeFirstCat = cats.slice();
+    removeFirstCat.shift();
+    return removeFirstCat;
+}
